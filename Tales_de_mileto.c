07137628dd26966b79bd96b0bd73759c0e349e20
Vicente Garcia Andrade <vicgarci@c1r7s4.42madrid.com>
2023-06-05 10:37:35 +0200

    Añadido descrip deomocrito

--- Tales_de_mileto.c
+++ Tales_de_mileto.c
@@ -6,20 +6,21 @@
 /*   By: vicgarci <vicgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 16:37:47 by vicgarci          #+#    #+#             */
-/*   Updated: 2023/06/05 10:25:44 by vicgarci         ###   ########.fr       */
+/*   Updated: 2023/06/05 10:31:15 by vicgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "laplace.h"
 
 /*
-Tales de mileto esta considerado el primer filosofo documentado en la historia
-humana, nativo de Mileto, una polis griega ubicada en la anatolia, se le
-atribuyen multiples aportaciones, pero lo más importante es aplicar el uso de
-la razón para encontrar una respuesta a las grandes preguntas.
+@par Tales de mileto esta considerado el primer filosofo documentado en la
+historia humana, nativo de Mileto, una polis griega ubicada en la anatolia, se 
+le atribuyen multiples aportaciones, pero lo más importante es aplicar el 
+uso de la razón para encontrar una respuesta a las grandes preguntas.
 
 Aun así el hecho de que no se perserve ningún documento directo suyo, hace que
 sea dificil atribuirle correctamente todos sus hallazgos
+@brief Basicamente es una abstracción del main, pero con otro nombre ^^
 */
 static void	tales_de_mileto(int argc, char **argv)
 {
--- democrito.h
+++ democrito.h
@@ -6,12 +6,25 @@
 /*   By: vicgarci <vicgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 14:43:11 by vicgarci          #+#    #+#             */
-/*   Updated: 2023/06/02 12:02:00 by vicgarci         ###   ########.fr       */
+/*   Updated: 2023/06/05 10:37:13 by vicgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEMOCRITO_H
 # define DEMOCRITO_H
+
+/*
+Democrito fue un filosofo griego que principalmente se e conoce por el atomismo,
+mediante esta visión  filosofica el mundo esta compuesto de atomos y vacio, se
+diferencia de la concepción moderna de atomo, desde su indivisibilidad hasta
+sus cualidades internas.
+
+por sus multiples trabajos matematicos, fisicos y antropologicos se le considera
+como uno de los padres de la ciencia moderna.
+
+Aun así es conocido por su personalidad risueña siendo apodado como "el filosofo
+que rie"
+*/
 
 typedef enum s_bool
 {
